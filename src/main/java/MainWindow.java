4e93b4cca3c4525702608ec762b9f00095099f37
Numetall <catnsinart@gmail.com>
2022-09-19 16:24:23 +0300

    snake-the game v1.0

--- src/main/java/MainWindow.java
+++ src/main/java/MainWindow.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class MainWindow {
+import javax.swing.*;
+import java.awt.*;
+
+public class MainWindow extends JFrame {
+    public MainWindow() {
+        setTitle("Snake-The Game");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(320,345);
+        setLocation(400,400);
+        add(new GameField());
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        MainWindow mainWindow = new MainWindow();
+    }
 }
